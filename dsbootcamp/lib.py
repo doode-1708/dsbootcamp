dc43183da43fc4583e4c357ae46b8947e16f4adc
Daniel Angerer <daniel.angerer@me.com>
2022-04-23 11:34:22 +0200

    new library including tests

---
+++ dsbootcamp/lib.py
@@ -0,0 +1,12 @@
+def try_me():
+    response = "I like the datascience bootcamp :-)"
+    print(f"{response}")
+
+    return response
+
+def another_useless_function():
+
+    return ":-D"
+
+if __name__ == 'main':
+    try_me()
---
+++ tests/test_lib.py
@@ -0,0 +1,6 @@
+from dsbootcamp.lib import try_me
+
+response = try_me()
+
+def test_right_response():
+    assert response == "I like the datascience bootcamp :-)"
